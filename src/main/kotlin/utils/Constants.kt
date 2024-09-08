a102d8f3a485b08b2aa0a236d4bd31a7361a52fa
ahmed elkomy <ahmedelkomy@gmail.com>
2024-09-08 08:09:45 +0300

    Constants class for variable not changes

---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,11 @@
+package utils
+
+
+object Constants {
+    const val PORT_NUMBER = 9999
+    const val SERVER_LISTENING = "Server started. Listening ..."
+    const val ENTER_YOUR_USER_NAME = "Enter your username: "
+    const val JOINED = "has joined the chat"
+    const val YOU_HAVE_LEFT = "You have left the chat."
+    const val CLIENT_DISCONNECTED = "has been disconnected."
+}
